4ea8ea3522738195fb5e0c0e40d2ab248c7ad952
alesperri00-source <alesperri00@gmail.com>
2026-04-23 20:40:45 +0200

    changed the indexing and now I save intermediate confs take 6

--- main.cpp
+++ main.cpp
@@ -61,7 +61,7 @@ std::vector<double> linspace(float start, float end, size_t points)
 const int num_points = 26;
 std::vector<double> betas = linspace(0.5, 3., num_points);
 // now I am running 1 thread per beta SO number of threads = number of betas
-const int number_of_threads = static_cast<int>(betas.size());
+const int number_of_threads = num_points;
 std::vector<std::vector<Vector3i>> polymer(number_of_threads);
 
 bool boundary_cond = 1; //enforces boundary conditions if 1
